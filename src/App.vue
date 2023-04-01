d75afbc0c52f51b3c8450dc3b9daec45fd04b49c
BogdanFV <fbogdan13@gmail.com>
2023-04-01 20:51:04 +0300

    few changes

--- src/App.vue
+++ src/App.vue
@@ -1,137 +1,151 @@
 <template>
   <div class="app-cover">
     <div class="lift-system">
-      <FloorsBlock :floors="floors" :activeButtons="activeButtons" @call-elevator="callElevator" />
-      <ShaftBlock :elevatorState="elevatorState" :arrived="arrived" :height="height" :floorsAmount="floorsAmount"
-        :marginBottom="marginBottom" :distance="distance" :displayText="displayText" :arrowState="arrowState" />
+      <div class="floors">
+        <div v-for="floor in floors" class="floor" :key="floor">
+          <button class="call-button" :class="{ 'active': activeButtons.includes(floor) }"
+            :style="{ 'background-color': activeButtons.includes(floor) ? 'red' : 'white' }" @click="callElevator(floor)">
+            {{ floor }}
+          </button>
+        </div>
+      </div>
+      <div class="shaft" :style="{ height: `${height * floorsAmount}px` }">
+        <div class="elevators">
+          <div v-for="(elevator, index)  in elevators" :key="elevator" :state="elevators[index].elevatorState"
+            :class="elevators[index].arrived ? 'elevator arrived' : 'elevator'" 
+            :style="{
+              marginBottom: elevators[index].marginBottom,
+              transitionProperty: elevators[index].marginBottom,
+              transitionDuration: `${elevators[index].distance}s`,
+              height: `${height}px`
+            }"
+          >
+          <div class="elevator-info">
+            <img alt="arrow" src="../src/assets/arrow-up.svg" :class="arrowState(index)">
+            <div class="display">{{ elevators[index].displayText }}</div>
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+
 <script>
-
-import FloorsBlock from '@/components/FloorsBlock.vue';
-import ShaftBlock from '@/components/ShaftBlock.vue';
-
 export default {
-  components: {
-    FloorsBlock,
-    ShaftBlock,
-  },
   data() {
     return {
-      activeButtons: [],
-      arrived: false,
-      displayText: '',
-      distance: null,
-      elevatorPosition: 0,
-      direction: 'idle',
-
-      floorsAmount: 8,
-      floors: [],
+      floorsAmount: 7,
       height: 100,
-      isMoving: false,
+      elevatorsAmount: 3,
 
-      marginBottom: sessionStorage.getItem('marginBottom') || 0,
-      targetFloor: null,
       queue: [],
+      activeButtons: [],
 
-      elevatorState: null,
+      floors: [],
+      elevators: [],
+      queueIsNotEmpty: false,
+
+      targetFloor: null,
     };
   },
   methods: {
-
-    fillFloorsArray() {
-      this.floors = Array.from({ length: this.floorsAmount }, (_, i) => this.floorsAmount - i);
+    fillElevatorsArray() {
+      this.elevators = Array.from({ length: this.elevatorsAmount }, () => ({
+        arrived: false,
+        displayText: 1,
+        distance: null,
+        elevatorPrevPosition: 0,
+        marginBottom: sessionStorage.getItem('marginBottom') || 0,
+        targetFloor: null,
+        elevatorState: 'idle'
+      }));
+      console.log(this.elevators);
     },
 
     callElevator(floor) {
-      if (this.queue.includes(floor) || this.elevatorPosition === (floor - 1)) {
+      if (this.queue.includes(floor) || this.elevatorPrevPosition === (floor - 1)) {
         return;
       }
-
       this.activeButtons.push(floor);
       this.queue.push(floor);
-      if (!this.isMoving) {
+      if (!this.queueIsNotEmpty) {
         this.moveElevator();
       }
     },
 
-    moveElevator(floor) {
+    moveElevator() {
       if (!this.queue.length) {
-        this.isMoving = false;
-        this.direction = 'idle';
+        this.queueIsNotEmpty = false;
         return;
       }
-      this.elevatorState = this.marginBottom > (this.targetFloor - 1) * this.height + 'px' ? 'moving down' : 'moving up';
-      this.movingUp = this.elevatorPosition > this.targetFloor - 1;
-      this.currentFloor = floor;
-      this.isMoving = true;
+      this.queueIsNotEmpty = true;
       this.targetFloor = this.queue.shift();
 
-
-      if (this.marginBottom > (this.targetFloor - 1) * this.height + 'px') {
-        this.direction = 'down';
-      } else {
-        this.direction = 'up';
-      }
-
-      this.marginBottom = (this.targetFloor - 1) * this.height + 'px';
-      this.distance = Math.abs((this.targetFloor - 1) - this.elevatorPosition);
-      this.elevatorPosition = (this.targetFloor - 1);
-      this.displayText = `${this.targetFloor}`;
-      sessionStorage.setItem('marginBottom', this.marginBottom);
-
+      this.elevators[0].elevatorState = parseInt(this.elevators[0].marginBottom) > (this.targetFloor - 1) * this.height ? 'moving down' : 'moving up';
+      this.elevators[0].marginBottom = (this.targetFloor - 1) * this.height + 'px';
+      this.elevators[0].distance = Math.abs((this.targetFloor - 1) - this.elevators[0].elevatorPrevPosition);
+      this.elevators[0].elevatorPrevPosition = (this.targetFloor - 1);
+      this.elevators[0].displayText = `${this.targetFloor}`;
 
       setTimeout(() => {
-        this.arrived = true;
+        this.elevators[0].arrived = true;
         const index = this.activeButtons.indexOf(this.targetFloor);
         if (index !== -1) {
           this.activeButtons.splice(index, 1);
         }
         setTimeout(() => {
-          this.arrived = false;
-          this.moveElevator(floor);
+          this.elevators[0].arrived = false;
+          this.moveElevator();
         }, 3000);
-      }, this.distance * 1000)
+      }, this.elevators[0].distance * 1000);
     },
 
     saveState() {
       const state = {
-        elevatorPosition: this.elevatorPosition,
-        queue: this.queue,
-        displayText: this.displayText,
-        targetFloor: this.targetFloor
+        marginBottom: this.elevators[0].marginBottom,
+        displayText: this.elevators[0].displayText,
+        targetFloor: this.elevators[0].targetFloor,
+        elevatorPrevPosition: this.elevators[0].elevatorPrevPosition
       };
       sessionStorage.setItem('liftSystemState', JSON.stringify(state));
     },
 
     handleQueue() {
-      if (!this.isMoving) {
+      if (!this.queueIsNotEmpty) {
         this.moveElevator();
       }
     },
 
-    arrowState() {
-      if (this.isMoving) {
-        if (this.direction === "up") {
-          return 'arrow-up'
-        } else if (this.direction === "down") {
-          return 'arrow-down'
+    arrowState(index) {
+      if (this.queueIsNotEmpty) {
+        switch (true) {
+          case (this.elevators[index].elevatorState === "moving up"):
+            return 'arrow-up'
+          case (this.elevators[index].elevatorState === "moving down"):
+            return 'arrow-down'
+          default:
+            return 'arrow'
         }
-      } else {
-        return 'arrow'
       }
-    }
+      return 'arrow'
+    },
+
+    fillFloorsArray() {
+      this.floors = Array.from({ length: this.floorsAmount }, (_, i) => this.floorsAmount - i);
+    },
 
   },
 
   mounted() {
+    this.fillElevatorsArray();
     this.fillFloorsArray();
     const savedState = JSON.parse(sessionStorage.getItem('liftSystemState'));
     if (savedState) {
-      this.elevatorPosition = savedState.elevatorPosition;
-      this.queue = savedState.queue;
-      this.displayText = savedState.displayText;
+      this.elevators[0].marginBottom = savedState.marginBottom;
+      this.elevatorPrevPosition = savedState.elevatorPrevPosition;
+      this.elevators[0].displayText = savedState.displayText;
       this.targetFloor = savedState.targetFloor;
     }
     window.addEventListener('beforeunload', this.saveState);
@@ -196,17 +210,11 @@ body {
   width: 16vh;
 }
 
-.elevator {
-  position: absolute;
-  bottom: 0;
-  height: 100px;
-  width: 16vh;
-}
-
-.shaft {
-  width: 16vh;
-  position: relative;
-  background: orange;
+.elevators {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  height: 100%;
 }
 
 .elevator {
@@ -214,9 +222,17 @@ body {
   justify-content: center;
   align-items: center;
   background-color: rgb(23, 23, 29);
+  bottom: 0;
+  width: 16vh;
+  border-left: 1px solid white;
+  border-right: 1px solid white;
 }
 
-.elevator-container {
+.shaft {
+  background: orange;
+}
+
+.elevator-info {
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -257,4 +273,5 @@ body {
 
 .arrived {
   animation: blink 0.5s ease-in-out 6;
-}</style>
+}
+</style>
